4b66552172f8715ba7a97fe972765a6faa3839d1
jnyjny <erik.oshaughnessy@gmail.com>
2019-12-13 10:57:22 -0600

    Refactored some of Scrutinizer into token
    Added pyliter.token.StyledToken which is a duck type for
    tokenize.TokenInfo. StyledToken supports style dictionaries and saves
    the raw string and allows the string attribute to be updated (unlike
    TokenInfo which is a namedtuple). Expanded the exact types range of
    types to include things like KEYWORD, BUILTIN, DOCSTRING, FNAME,
    CNAME, INAME, LVAL, SELF and more.
    
    Lastly, added a ANSIStyledToken subclass of StyledToken that overrides
    the string property to apply the style dictionary to the string. The
    idea here is to support future StyledToken subclasses that do not
    rely on ANSI escape codes to style the text.

--- pyliter/scrutinizer.py
+++ pyliter/scrutinizer.py
@@ -1,11 +1,7 @@
 """
 """
 
-import click
-import tokenize
-import keyword
-
-import builtins
+from .token import ANSIStyledToken
 
 
 class Scrutinizer:
@@ -15,90 +11,10 @@ class Scrutinizer:
         :param dict style:
         """
         self.input = input_file
-        self.style = style
+        self.style = style or {}
 
     def __str__(self):
-        return tokenize.untokenize(self.tokens).decode()
-
-    @property
-    def builtins(self) -> list:
-        """List of strings that are considered builtins.
-        """
-        try:
-            return self._builtins
-        except AttributeError:
-            pass
-        self._builtins = dir(builtins)
-        return self._builtins
-
-    @property
-    def definitions(self) -> list:
-        """List of python strings which constitue "definitions".
-        """
-        try:
-            return self._definitions
-        except AttributeError:
-            pass
-        self._definitions = ["def", "class"]
-        return self._definitions
-
-    def classify(self, token: tokenize.TokenInfo, previous: tokenize.TokenInfo) -> None:
-        """Attaches a 'name' attribute to token based.
-
-        :param tokenize.TokenInfo token: 
-        :param tokenize.TokenInfo previous: 
-        """
-
-        token.name = tokenize.tok_name[token.type]
-
-        if token.exact_type == tokenize.EQUAL:
-            try:
-                previous.name = "LVAL"
-            except AttributeError:
-                pass
-
-        if token.exact_type == tokenize.COMMENT:
-            token.name = "COMMENT"
-            return
-
-        if keyword.iskeyword(token.string):
-            token.name = "KEYWORD"
-
-        if token.string in self.builtins:
-            token.name = "BUILTINS"
-
-        if token.string.startswith('"""') or token.string.startswith("'''"):
-            token.name = "DOCSTRING"
-
-        if token.string.startswith("_"):
-            token.name = "PRIVATE"
-
-        try:
-            if previous.string in self.definitions:
-                token.name = "DEFINITION"
-        except AttributeError:
-            pass
-
-    def transform(self, token) -> tokenize.TokenInfo:
-        """Given a token, the scrutinizer checks to see if the token
-        matches configured style classifications. If it does, a new
-        token with updated string attribute is created and returned.
-        Otherwise, the original unchanged token is returned.
-
-        :param tokenize.TokenInfo token:
-        :return: tokenize.TokenInfo
-        """
-
-        style = {}
-        for classifier in [token.name, token.string]:
-            style.update(self.style.get(classifier, {}))
-
-        if not style:
-            return token
-
-        styled_token = token._replace(string=click.style(token.string, **style))
-        styled_token.name = token.name
-        return styled_token
+        return ANSIStyledToken.untokenize(self.tokens)
 
     @property
     def tokens(self) -> list:
@@ -109,15 +25,6 @@ class Scrutinizer:
         except AttributeError:
             pass
 
-        tokens = [None]
-        for token in tokenize.tokenize(self.input.readline):
-            self.classify(token, tokens[-1])
-            tokens.append(token)
-
-        self._tokens = []
-        for token in tokens[1:]:
-            if token.type != tokenize.ENCODING:
-                token = self.transform(token)
-            self._tokens.append(token)
+        self._tokens = list(ANSIStyledToken.tokenize(self.input.readline, self.style))
 
         return self._tokens
---
+++ pyliter/token.py
@@ -0,0 +1,215 @@
+"""
+"""
+
+import click
+import token as TOKEN
+import builtins
+import keyword
+
+from tokenize import tokenize as original_tokenize
+from tokenize import untokenize as original_untokenize
+from tokenize import EXACT_TOKEN_TYPES, tok_name
+
+
+_BUILTINS = dir(builtins)
+
+
+KEYWORD = 1101
+BUILTIN = 1102
+DEFINITIONS = 1103
+DOCSTRING = 1104
+FNAME = 1105
+CNAME = 1106
+INAME = 1107
+LVAL = 1108
+HIDDEN = 1109
+SELF = 1110
+
+tok_name[KEYWORD] = "KEYWORD"
+tok_name[BUILTIN] = "BUILTIN"
+tok_name[DEFINITIONS] = "DEFINITIONS"
+tok_name[DOCSTRING] = "DOCSTRING"
+tok_name[FNAME] = "FNAME"
+tok_name[CNAME] = "CNAME"
+tok_name[INAME] = "INAME"
+tok_name[LVAL] = "LVAL"
+tok_name[HIDDEN] = "HIDDEN"
+tok_name[SELF] = "SELF"
+
+
+class StyledToken:
+    """tokenize.TokenInfo compatible object with extended attributes.
+    """
+
+    @classmethod
+    def tokenize(cls, readline, style: dict = None) -> list:
+        """
+        """
+
+        style = style or {}
+        prev = None
+        tokens = []
+        for token_info in original_tokenize(readline):
+            prev = cls(*list(token_info._asdict().values()), prev=prev)
+            tokens.append(prev)
+            prev._resolve_upstream()
+
+        for token in tokens:
+            token.apply_style(style)
+            yield token
+
+    @staticmethod
+    def untokenize(tokens) -> str:
+        """
+        """
+        return original_untokenize(tokens).decode()
+
+    def __init__(
+        self, type: int, string: str, start: tuple, end: tuple, line: str, prev=None
+    ):
+        self.type = type
+        self.raw_string = string
+        self.start = start
+        self.end = end
+        self.line = line
+        self.prev = prev
+
+    @property
+    def exact_type(self) -> int:
+        try:
+            return self._exact_type
+        except AttributeError:
+            pass
+        self._exact_type = self.type
+        if self.type == TOKEN.OP:
+            try:
+                self._exact_type = EXACT_TOKEN_TYPES[self.string]
+            except KeyError:
+                pass
+        if keyword.iskeyword(self.raw_string):
+            self._exact_type = KEYWORD
+        if self.raw_string in _BUILTINS:
+            self._exact_type = BUILTIN
+        if self.raw_string.startswith(('"""', "'''")):
+            self._exact_type = DOCSTRING
+        if self.raw_string.startswith("_"):
+            self._exact_type = HIDDEN
+        try:
+            if self.prev.raw_string == "def":
+                self._exact_type = FNAME
+            if self.prev.raw_string == "class":
+                self._exact_type = CNAME
+            if self.prev.raw_string == "import":
+                self._exact_type = INAME
+        except AttributeError:
+            pass
+
+        return self._exact_type
+
+    @exact_type.setter
+    def exact_type(self, new_type: int) -> None:
+        self._exact_type = new_type
+
+    @property
+    def string(self) -> str:
+        try:
+            return self._string
+        except AttributeError:
+            pass
+        self._string = self.raw_string
+        return self._string
+
+    @string.setter
+    def string(self, new_string: str) -> None:
+        self._string = new_string
+
+    def __repr__(self) -> str:
+        msg = "type={}[{:2d}], et={:2d}, name='{}', string='{}', prev={}, style={}"
+        return msg.format(
+            tok_name[self.type],
+            self.type,
+            self.exact_type,
+            self.name,
+            self.string if "\n" not in self.string else "...",
+            self.prev.name if self.prev else "None",
+            self.style,
+        )
+
+    def __str__(self) -> str:
+        return self.string
+
+    def _resolve_upstream(self):
+
+        if self.exact_type == TOKEN.EQUAL:
+            pt = self.prev
+            while pt:
+                t, pt = pt, pt.prev
+                if t.exact_type == TOKEN.DOT:
+                    t.exact_type = LVAL
+                    continue
+                if t.type == TOKEN.NAME:
+                    t.exact_type = LVAL
+                    continue
+                if t.type == TOKEN.OP:
+                    continue
+                break
+
+    @property
+    def name(self):
+        try:
+            return self._name
+        except AttributeError:
+            pass
+
+        try:
+            self._name = tok_name[self.exact_type]
+        except KeyError as error:
+            print(f"missing exact type name {self.exact_type}")
+            raise
+        return self._name
+
+    @property
+    def style(self):
+        try:
+            return self._style
+        except AttributeError:
+            pass
+        self._style = {}
+        return self._style
+
+    @property
+    def prev(self):
+        try:
+            return self._prev
+        except AttributeError:
+            pass
+        self._prev = None
+        return self._prev
+
+    @prev.setter
+    def prev(self, previous_token):
+        self._prev = previous_token
+
+    def apply_style(self, style):
+        self.style.update(style.get(self.name, {}))
+
+    def __len__(self):
+        return 5
+
+    def __iter__(self):
+        return iter([self.type, self.string, self.start, self.end, self.line])
+
+
+class ANSIStyledToken(StyledToken):
+    @property
+    def string(self):
+        try:
+            return self._string
+        except AttributeError:
+            pass
+
+        if self.type not in [TOKEN.OP, TOKEN.NAME, TOKEN.STRING, TOKEN.COMMENT]:
+            self._string = self.raw_string
+        else:
+            self._string = click.style(self.raw_string, **self.style)
+        return self._string
