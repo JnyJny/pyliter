0060e2015afe06e2e508e03832cdddd593030cf2
JnyJny <erik.oshaughnessy@gmail.com>
2019-12-29 19:30:58 -0600

    added more crayola-style colors to the catalog

--- pyliter/resources/__init__.py
+++ pyliter/resources/__init__.py
@@ -1,4 +1,15 @@
-color_catalog_files = ["css_colors.yaml"]
+color_catalog_files = [
+    "css_colors.yaml",
+    "crayola_bright.yaml",
+    "crayola_colors.yaml",
+    "crayola_fluorescent.yaml",
+    "crayola_fragrance.yaml",
+    "crayola_jewelry.yaml",
+    "crayola_magic.yaml",
+    "crayola_metallic.yaml",
+    "crayola_scary.yaml",
+    "crayola_silver.yaml",
+]
 
 
 __all__ = ["color_catalog_files"]
