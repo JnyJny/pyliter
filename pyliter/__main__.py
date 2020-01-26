329fb3fe1322621bfd405629950f6e33a22fd3df
eoshaugh <erik.oshaughnessy@intel.com>
2020-01-26 14:24:26 -0600

    Switched from click to typer
    - removed click decorators
    - added typer
    - re-wrote CLI function with typed arguments

--- pyliter/__main__.py
+++ pyliter/__main__.py
@@ -2,110 +2,50 @@
 
 """
 
-import click
-import io
-import sys
-import yaml
 
+import io
+import pathlib
+import sys
+import typer
+import yaml
 
 from . import VERSION
 from .render import PythonRender
 from .style_book import StyleBook
 
+pyliter_cli = typer.Typer()
 
-@click.command()
-@click.argument(
-    "input-file", type=click.File(mode="rb"), default=sys.stdin,
-)
-@click.option(
-    "-o",
-    "--output-file",
-    type=click.Path(),
-    default=None,
-    help="Creates a PNG with the supplied path.",
-)
-@click.option(
-    "-l",
-    "--start-line",
-    default=0,
-    help="Line number to begin display.",
-    show_default=True,
-)
-@click.option(
-    "-n",
-    "--line-count",
-    default=10,
-    help="Number of lines to display.",
-    show_default=True,
-)
-@click.option(
-    "-N",
-    "--no-line-numbers",
-    is_flag=True,
-    default=False,
-    help="Disable line numbers in output.",
-    show_default=True,
-)
-@click.option(
-    "-p",
-    "--preview",
-    is_flag=True,
-    default=False,
-    help="Previews output in window.",
-    show_default=True,
-)
-@click.option(
-    "-t",
-    "--transparent",
-    is_flag=True,
-    default=False,
-    help="Write PNG with transparency.",
-    show_default=True,
-)
-@click.option(
-    "-s",
-    "--style-name",
-    type=click.STRING,
-    default="default",
-    help="Style to apply to input file.",
-    show_default=True,
-)
-@click.option(
-    "-f",
-    "--font-name",
-    type=click.STRING,
-    default="courier",
-    help="Font name.",
-    show_default=True,
-)
-@click.option(
-    "-S",
-    "--font-size",
-    type=click.INT,
-    default=24,
-    help="Font size",
-    show_default=True,
-)
-@click.option(
-    "-L",
-    "--list-styles",
-    is_flag=True,
-    default=False,
-    help="List available styles and exits.",
-)
-@click.version_option(VERSION)
-def pyliter_cli(
-    input_file,
-    output_file,
-    start_line,
-    line_count,
-    no_line_numbers,
-    preview,
-    transparent,
-    style_name,
-    font_name,
-    font_size,
-    list_styles,
+
+def print_version(ctx, param, value):
+    if not value:
+        return
+    typer.secho("version ", fg="red", nl=False)
+    typer.secho(VERSION, fg="green")
+    raise typer.Exit()
+
+
+@pyliter_cli.command()
+def main(
+    input_file: pathlib.Path = typer.Argument("-", allow_dash=True),
+    output_file: pathlib.Path = typer.Argument(None),
+    start_line: int = typer.Option(0, "--start-line", "-l", show_default=True),
+    line_count: int = typer.Option(10, "--line-count", "-c", show_default=True),
+    no_line_numbers: bool = typer.Option(False, "--no-line-numbers", "-N"),
+    preview: bool = typer.Option(
+        False, "--preview", "-p", help="Preview rendered code in a window."
+    ),
+    transparent: bool = typer.Option(
+        False, "--transparent", "-t", help="Enable transparent background."
+    ),
+    style_name: str = typer.Option(
+        "default", "--style", "-S", help="Colors to style text."
+    ),
+    font_name: str = typer.Option("courier", "--font-name", "-f", show_default=True),
+    font_size: int = typer.Option(24, "--font-size", "-s", show_default=True),
+    list_styles: bool = typer.Option(False, "--list-styles", "-L", help=""),
+    version: bool = typer.Option(
+        False, "--version", "-v", is_eager=True, callback=print_version
+    ),
 ):
     """Python syntax highlighting
 
@@ -127,8 +67,10 @@ def pyliter_cli(
         print_help(str(error))
         return
 
-    if input_file == sys.stdin:
+    if input_file.name == "-":
         input_file = io.BytesIO(sys.stdin.buffer.read())
+    else:
+        input_file = input_file.open("rb")
 
     if not output_file:
         preview = True
@@ -159,8 +101,11 @@ def print_help(msg: str) -> None:
     print(f"\n{msg}\n")
 
 
-@click.command()
-def pyliter_style_cli():
+pyliter_style_cli = typer.Typer()
+
+
+@pyliter_style_cli.command()
+def pyliter_style():
     """Manage pyliter styles.
     """
     pass
