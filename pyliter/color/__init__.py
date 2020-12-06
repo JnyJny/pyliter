ba36743df5f320832bf139360d3e452c4a677809
JnyJny <erik.oshaughnessy@gmail.com>
2020-12-06 13:42:23 -0600

    Removed janky color implementation

--- pyliter/color/__init__.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import collections
-
-from .color import Color
-from .catalog import COLORS_BY_NAME
-from .exceptions import ColorNameNotFound
-
-__all__ = ["Color", "COLORS_BY_NAME", "ColorNameNotFound"]
--- pyliter/color/exceptions.py
+++
@@ -1,6 +0,0 @@
-"""
-"""
-
-
-class ColorNameNotFound(Exception):
-    pass
